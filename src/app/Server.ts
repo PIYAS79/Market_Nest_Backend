a7e824dfa0c8e9a21ab4e5453eaa773ce5d4b774
S M Piyas Mahamude Alif <piyasmahmudealif@gmail.com>
2024-12-09 00:21:49 +0600

    customize the admin info

--- src/app/Server.ts
+++ src/app/Server.ts
@@ -17,10 +17,10 @@ async function main() {
         const isAdminExist = await entityManager.findOne(User_Entity, { where: { role: "ADMIN" } });
         if (!isAdminExist) {
             await entityManager.insert(User_Entity, {
-                email: "super@gmail.com",
-                image: "https://www.metroworldnews.com.br/resizer/v2/44V5ZRVUOBBVXAMIRQBBEEI6TI.webp?smart=true&auth=328e8b61a04d63aa85c79a187bab82f8c7096b5857d7c94d7a1c56f73571eaa9&width=1200&height=630",
-                name: "Donand Trump",
-                password: "blackops",
+                email: "sumaiya@gmail.com",
+                image: "https://static.vecteezy.com/system/resources/thumbnails/008/974/656/small_2x/cute-kid-girl-holding-bubble-milk-tea-hand-drawn-cartoon-character-illustration-vector.jpg",
+                name: "Jannatul Ferdaus Sumaiya",
+                password: "222156423",
                 role: "ADMIN",
                 address:"Dhaka, Bangladesh",
                 contact_number:"0123654789",
